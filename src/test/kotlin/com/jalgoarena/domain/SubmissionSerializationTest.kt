04e8cc35c89cae59fa6dc17b15ee1419d0259d39
Jacek Spólnik <jacek.spolnik@gmail.com>
2018-07-29 08:32:32 +0200

    - make token mandatory - switch submissionTime to LocalDateTime

--- src/test/kotlin/com/jalgoarena/domain/SubmissionSerializationTest.kt
+++ src/test/kotlin/com/jalgoarena/domain/SubmissionSerializationTest.kt
@@ -1,12 +1,16 @@
 package com.jalgoarena.domain
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+import com.fasterxml.jackson.databind.DeserializationFeature
+import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import org.assertj.core.api.Assertions.assertThat
 import org.intellij.lang.annotations.Language
 import org.junit.Before
 import org.junit.Test
 import org.springframework.boot.test.json.JacksonTester
+import java.time.LocalDateTime
+import java.time.Month
 
 class SubmissionSerializationTest {
 
@@ -15,6 +19,9 @@ class SubmissionSerializationTest {
     @Before
     fun setup() {
         val objectMapper = jacksonObjectMapper()
+        objectMapper.findAndRegisterModules()
+        objectMapper.disable(DeserializationFeature.ADJUST_DATES_TO_CONTEXT_TIME_ZONE)
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
         JacksonTester.initFields(this, objectMapper)
     }
 
@@ -30,25 +37,11 @@ class SubmissionSerializationTest {
                 .isEqualTo(SUBMISSION)
     }
 
-    @Test
-    fun make_sure_queue_submission_can_be_deserialized() {
-        val queueSubmission = QueueSubmission(
-                SUBMISSION.sourceCode,
-                SUBMISSION.userId,
-                SUBMISSION.submissionId,
-                SUBMISSION.submissionTime,
-                SUBMISSION.problemId,
-                SUBMISSION.token,
-                "WAITING"
+    companion object {
+        private val SUBMISSION_TIME = LocalDateTime.of(
+                2018, Month.JULY, 29, 8, 1, 1
         )
 
-        val json = jacksonObjectMapper().writeValueAsString(queueSubmission)
-        val submission = jacksonObjectMapper().readValue<Submission>(json, Submission::class.java)
-
-        assertThat(submission.statusCode).isEqualTo("WAITING")
-    }
-
-    companion object {
         private val SUBMISSION = Submission(
                 problemId = "fib",
                 elapsedTime = 435.212,
@@ -59,10 +52,10 @@ class SubmissionSerializationTest {
                 submissionId = "2",
                 consumedMemory = 10L,
                 errorMessage = null,
-                submissionTime = "",
+                submissionTime = SUBMISSION_TIME,
                 passedTestCases = 1,
                 failedTestCases = 0,
-                token = null
+                token = "dummy_token"
         )
 
         @Language("JSON")
@@ -74,22 +67,12 @@ class SubmissionSerializationTest {
   "userId": "0-0",
   "id": 2,
   "submissionId": "2",
-  "submissionTime": "",
+  "submissionTime": "2018-07-29T08:01:01",
   "consumedMemory": 10,
   "passedTestCases": 1,
-  "failedTestCases": 0
+  "failedTestCases": 0,
+  "token": "dummy_token"
 }
 """
     }
-
-    @JsonIgnoreProperties(ignoreUnknown = true)
-    data class QueueSubmission(
-            val sourceCode: String,
-            val userId: String,
-            val submissionId: String,
-            val submissionTime: String,
-            val problemId: String,
-            val token: String?,
-            val statusCode: String
-    )
 }
