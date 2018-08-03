276a3ddd7b8dde59f933b235f0c8e44c45d931ac
Jacek Spólnik <jacek.spolnik@gmail.com>
2018-08-03 13:44:20 +0200

    - improve resilience of ranking service

--- src/main/kotlin/com/jalgoarena/web/RankingController.kt
+++ src/main/kotlin/com/jalgoarena/web/RankingController.kt
@@ -2,11 +2,16 @@ package com.jalgoarena.web
 
 import com.jalgoarena.data.ProblemsRepository
 import com.jalgoarena.data.SubmissionsRepository
+import com.jalgoarena.domain.ProblemRankEntry
+import com.jalgoarena.domain.RankEntry
 import com.jalgoarena.domain.SolvedRatioEntry
 import com.jalgoarena.domain.Submission
 import com.jalgoarena.ranking.RankingCalculator
 import com.jalgoarena.ranking.RankingCalculator.Companion.acceptedWithBestTimes
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.dao.DataAccessException
+import org.springframework.transaction.TransactionException
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
@@ -19,27 +24,42 @@ class RankingController(
         @Autowired private val submissionsRepository: SubmissionsRepository
 ) {
 
+    private val logger = LoggerFactory.getLogger(this.javaClass)
+
     @GetMapping("/ranking", produces = ["application/json"])
-    fun ranking() = rankingCalculator.ranking(
-            users = usersClient.findAllUsers().filter {
-                it.username.toLowerCase() != "admin"
-            },
-            submissions = acceptedWithBestTimes(submissionsRepository.findAll()),
-            problems = problemsRepository.findAll()
-    )
+    fun ranking(): List<RankEntry> = try {
+        rankingCalculator.ranking(
+                users = usersClient.findAllUsers().filter {
+                    it.username.toLowerCase() != "admin"
+                },
+                submissions = acceptedWithBestTimes(submissionsRepository.findAll()),
+                problems = problemsRepository.findAll()
+        )
+    } catch (e: TransactionException) {
+        logger.error("Cannot connect to database", e)
+        listOf()
+    }
 
     @GetMapping("/ranking/problem/{problemId}", produces = ["application/json"])
-    fun problemRanking(@PathVariable problemId: String) =
+    fun problemRanking(@PathVariable problemId: String) = try {
             rankingCalculator.problemRanking(
                     problemId = problemId,
                     users = usersClient.findAllUsers().filter {
                         it.username.toLowerCase() != "admin"
                     },
                     problems = problemsRepository.findAll())
+    } catch (e: DataAccessException) {
+        logger.error("Cannot connect to database", e)
+        listOf<ProblemRankEntry>()
+    }
 
     @GetMapping("/solved-ratio", produces = ["application/json"])
-    fun submissionsSolvedRatio() =
+    fun submissionsSolvedRatio() = try {
             calculateSubmissionsSolvedRatioAndReturnIt(submissionsRepository.findAll())
+    } catch (e: TransactionException) {
+        logger.error("Cannot connect to database", e)
+        listOf<SolvedRatioEntry>()
+    }
 
     private fun calculateSubmissionsSolvedRatioAndReturnIt(submissions: List<Submission>) =
             submissions
