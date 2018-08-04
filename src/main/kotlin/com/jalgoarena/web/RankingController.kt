df683ac0147efb958300156b99abbdc34864b2a4
Jacek Spólnik <jacek.spolnik@gmail.com>
2018-08-04 07:11:01 +0200

    - add rest endpoint to query ranking start date

--- src/main/kotlin/com/jalgoarena/web/RankingController.kt
+++ src/main/kotlin/com/jalgoarena/web/RankingController.kt
@@ -2,10 +2,7 @@ package com.jalgoarena.web
 
 import com.jalgoarena.data.ProblemsRepository
 import com.jalgoarena.data.SubmissionsRepository
-import com.jalgoarena.domain.ProblemRankEntry
-import com.jalgoarena.domain.RankEntry
-import com.jalgoarena.domain.SolvedRatioEntry
-import com.jalgoarena.domain.Submission
+import com.jalgoarena.domain.*
 import com.jalgoarena.ranking.RankingCalculator
 import com.jalgoarena.ranking.RankingCalculator.Companion.acceptedWithBestTimes
 import org.slf4j.LoggerFactory
@@ -32,9 +29,7 @@ class RankingController(
     @GetMapping("/ranking", produces = ["application/json"])
     fun ranking(): List<RankEntry> = try {
         rankingCalculator.ranking(
-                users = usersClient.findAllUsers().filter {
-                    it.username.toLowerCase() != "admin"
-                },
+                users = allUsersWithoutAdmin(),
                 submissions = acceptedWithBestTimes(submissionsRepository.findAll()),
                 problems = problemsRepository.findAll()
         )
@@ -48,9 +43,7 @@ class RankingController(
         val tillDate = LocalDate.parse(date, YYYY_MM_DD).plusDays(1).atStartOfDay()
 
         rankingCalculator.ranking(
-                users = usersClient.findAllUsers().filter {
-                    it.username.toLowerCase() != "admin"
-                },
+                users = allUsersWithoutAdmin(),
                 submissions = acceptedWithBestTimes(submissionsRepository.findBySubmissionTimeLessThan(tillDate)),
                 problems = problemsRepository.findAll()
         )
@@ -59,22 +52,44 @@ class RankingController(
         listOf()
     }
 
+    @GetMapping("/ranking/startDate", produces = ["application/json"])
+    fun rankingStartDate() = try {
+        val submission = submissionsRepository.findAll().minBy { it.submissionTime }
+
+        if (submission == null) {
+            yesterday()
+        } else {
+            aDayBefore(submission.submissionTime)
+        }
+
+    } catch (e: TransactionException) {
+        logger.error("Cannot connect to database", e)
+        yesterday()
+    }
+
+    private fun aDayBefore(submissionTime: LocalDateTime) =
+            submissionTime.minusDays(1).format(YYYY_MM_DD)
+
+    private fun yesterday() = LocalDateTime.now().minusDays(1).format(YYYY_MM_DD)
+
     @GetMapping("/ranking/problem/{problemId}", produces = ["application/json"])
     fun problemRanking(@PathVariable problemId: String) = try {
-            rankingCalculator.problemRanking(
-                    problemId = problemId,
-                    users = usersClient.findAllUsers().filter {
-                        it.username.toLowerCase() != "admin"
-                    },
-                    problems = problemsRepository.findAll())
+        rankingCalculator.problemRanking(
+                problemId = problemId,
+                users = allUsersWithoutAdmin(),
+                problems = problemsRepository.findAll())
     } catch (e: DataAccessException) {
         logger.error("Cannot connect to database", e)
         listOf<ProblemRankEntry>()
     }
 
+    private fun allUsersWithoutAdmin(): List<User> = usersClient.findAllUsers().filter {
+        it.username.toLowerCase() != "admin"
+    }
+
     @GetMapping("/solved-ratio", produces = ["application/json"])
     fun submissionsSolvedRatio() = try {
-            calculateSubmissionsSolvedRatioAndReturnIt(submissionsRepository.findAll())
+        calculateSubmissionsSolvedRatioAndReturnIt(submissionsRepository.findAll())
     } catch (e: TransactionException) {
         logger.error("Cannot connect to database", e)
         listOf<SolvedRatioEntry>()
