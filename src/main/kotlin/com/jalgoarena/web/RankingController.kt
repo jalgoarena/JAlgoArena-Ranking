ad3dd2ac3e15be5209dd58c4ebb27bd1912f5f57
Jacek Spólnik <jacek.spolnik@gmail.com>
2018-08-09 08:21:20 +0200

    - no need to filter admin anymore

--- src/main/kotlin/com/jalgoarena/web/RankingController.kt
+++ src/main/kotlin/com/jalgoarena/web/RankingController.kt
@@ -24,18 +24,19 @@ open class RankingController(
     @Cacheable("ranking")
     @GetMapping("/ranking", produces = ["application/json"])
     open fun ranking() = rankingCalculator.ranking(
-            users = allUsersWithoutAdmin(),
+            users = usersClient.findAllUsers(),
             submissions = acceptedWithBestTimes(submissionsClient.findAll()),
             problems = problemsClient.findAll()
     )
 
     @Cacheable("rankingTillDate", key = "#date")
     @GetMapping("/ranking/{date}", produces = ["application/json"])
-    open fun rankingTillDate(@PathVariable date: String) = rankingCalculator.ranking(
-            users = allUsersWithoutAdmin(),
-            submissions = acceptedWithBestTimes(submissionsClient.findBySubmissionTimeLessThan(date)),
-            problems = problemsClient.findAll()
-    )
+    open fun rankingTillDate(@PathVariable date: String) =
+            rankingCalculator.ranking(
+                    users = usersClient.findAllUsers(),
+                    submissions = acceptedWithBestTimes(submissionsClient.findBySubmissionTimeLessThan(date)),
+                    problems = problemsClient.findAll()
+            )
 
     @Cacheable("startDate")
     @GetMapping("/ranking/startDate", produces = ["application/json"])
@@ -51,26 +52,23 @@ open class RankingController(
 
     @Cacheable("problemRanking", key = "#problemId")
     @GetMapping("/ranking/problem/{problemId}", produces = ["application/json"])
-    open fun problemRanking(@PathVariable problemId: String) = rankingCalculator.problemRanking(
-            problemId = problemId,
-            users = allUsersWithoutAdmin(),
-            problems = problemsClient.findAll()
-    )
+    open fun problemRanking(@PathVariable problemId: String) =
+            rankingCalculator.problemRanking(
+                    problemId = problemId,
+                    users = usersClient.findAllUsers(),
+                    problems = problemsClient.findAll()
+            )
 
     @Cacheable("solvedRatio")
     @GetMapping("/solved-ratio", produces = ["application/json"])
     open fun submissionsSolvedRatio() =
-        calculateSubmissionsSolvedRatioAndReturnIt(submissionsClient.findAll().filter { it.statusCode == "ACCEPTED" })
+            calculateSubmissionsSolvedRatioAndReturnIt(submissionsClient.findAll().filter { it.statusCode == "ACCEPTED" })
 
     private fun aDayBefore(submissionTime: LocalDateTime) =
             submissionTime.minusDays(1).format(YYYY_MM_DD)
 
     private fun yesterday() =
             LocalDateTime.now().minusDays(1).format(YYYY_MM_DD)
-
-    private fun allUsersWithoutAdmin(): List<User> = usersClient.findAllUsers().filter {
-        it.username.toLowerCase() != "admin"
-    }
 
     private fun calculateSubmissionsSolvedRatioAndReturnIt(submissions: List<Submission>) =
             submissions
